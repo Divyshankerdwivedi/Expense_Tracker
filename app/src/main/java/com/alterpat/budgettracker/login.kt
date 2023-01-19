0aca5437190df3a23ccb5ccbfd1ead232d0a5723
Divyshankerdwivedi <76654813+Divyshankerdwivedi@users.noreply.github.com>
2023-01-20 02:25:54 +0530

    Initial commit

---
+++ app/src/main/java/com/alterpat/budgettracker/login.kt
@@ -0,0 +1,11 @@
+package com.alterpat.budgettracker
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class login : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.login)
+    }
+}
